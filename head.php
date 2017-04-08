bcb191f3a5eb80a24e74088e84c21181d7db4ceb
passefast <997698141@qq.com>
2017-04-08 10:40:07 +0800

    2

---
+++ head.php
@@ -0,0 +1,105 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<div class="container">
+	<div class="row clearfix">
+		<div class="col-md-12 column">
+			<nav class="navbar navbar-default" role="navigation">
+				<div class="navbar-header">
+					 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button> <a class="navbar-brand" href="#">Brand</a>
+				</div>
+				
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li class="active">
+							 <a href="#">Link</a>
+						</li>
+						<li>
+							 <a href="#">Link</a>
+						</li>
+						<li class="dropdown">
+							 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
+							<ul class="dropdown-menu">
+								<li>
+									 <a href="#">Action</a>
+								</li>
+								<li>
+									 <a href="#">Another action</a>
+								</li>
+								<li>
+									 <a href="#">Something else here</a>
+								</li>
+								<li class="divider">
+								</li>
+								<li>
+									 <a href="#">Separated link</a>
+								</li>
+								<li class="divider">
+								</li>
+								<li>
+									 <a href="#">One more separated link</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+					<form class="navbar-form navbar-left" role="search">
+						<div class="form-group">
+							<input type="text" class="form-control" />
+						</div> <button type="submit" class="btn btn-default">Submit</button>
+					</form>
+					<ul class="nav navbar-nav navbar-right">
+						<li>
+							 <a href="#">Link</a>
+						</li>
+						<li class="dropdown">
+							 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
+							<ul class="dropdown-menu">
+								<li>
+									 <a href="#">Action</a>
+								</li>
+								<li>
+									 <a href="#">Another action</a>
+								</li>
+								<li>
+									 <a href="#">Something else here</a>
+								</li>
+								<li class="divider">
+								</li>
+								<li>
+									 <a href="#">Separated link</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+				
+			</nav>
+			<div class="tabbable" id="tabs-564861">
+				<ul class="nav nav-tabs">
+					<li class="active">
+						 <a href="#panel-425905" data-toggle="tab">Section 1</a>
+					</li>
+					<li>
+						 <a href="#panel-192266" data-toggle="tab">Section 2</a>
+					</li>
+				</ul>
+				<div class="tab-content">
+					<div class="tab-pane active" id="panel-425905">
+						<p>
+							I'm in Section 1.
+						</p>
+					</div>
+					<div class="tab-pane" id="panel-192266">
+						<p>
+							Howdy, I'm in Section 2.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
--- zhuce.php
+++ zhuce.php
@@ -8,7 +8,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
     <link type="text/css" rel="stylesheet" href="css/login.css">
     <script type="text/javascript" src="js/jquery.min.js"></script>
-
+<script type="text/javascript" language="javascript" src="jquery.js"></script>  
+    <script type="text/javascript" language="javascript">   
+          
+        function fun(n) {  
+            var url = "sever.php"; 
+			var phone=getElementById(num).value;
+			var youxiang=getElementById(email).value;
+			var mima=getElementById(password).value;
+            var data = {  
+                action : n.value, 
+				phoneinfo:phone,
+				emailinfo:youxiang,
+				miaminfo:mima
+            };  
+            jQuery.post(url, data, callback);  
+        }  
+        function callback(data) {  
+            alert(data);  
+        }  
+    </script> 
 </head>
 <body class="login_bj" >
 
@@ -19,13 +38,13 @@
         	<div class="bj_bai">
             <h3>欢迎注册</h3>
        	  	  <form action="" method="get">
-                <input name="" type="text" class="kuang_txt phone" placeholder="手机号">
-                <input name="" type="text" class="kuang_txt email" placeholder="邮箱">
-                <input name="" type="text" class="kuang_txt possword" placeholder="密码">
+                <input name="" type="text" class="kuang_txt phone" placeholder="手机号" id="num">
+                <input name="" type="text" class="kuang_txt email" placeholder="邮箱" id="email">
+                <input name="" type="text" class="kuang_txt possword" placeholder="密码" id="password">
            
                 <div>
 				</div>
-                <input name="注册" type="button" class="btn_zhuce" value="注册">
+                <input name="注册" type="button" class="btn_zhuce" value="注册" onclick="fun(this)">
                 
                 </form>
             </div>
@@ -34,17 +53,14 @@
                 <a href="#" class="zhuce_qq">QQ注册</a>
                 <a href="#" class="zhuce_wb">微博注册</a>
                 <a href="#" class="zhuce_wx">微信注册</a>
-                <p>已有账号？<a href="login.html">立即登录</a></p>
+                <p>已有账号？<a href="login.php">立即登录</a></p>
             
             </div>
         </div>
     </div>
 
 </div>
-    
-<div style="text-align:center;">
-<p>来源:<a href="http://down.admin5.com/" target="_blank">A5源码</a></p>
-</div>
+
 
 </body>
 </html>
