a811ba531387eacac4116c7a6c5af61c61f42c84
“D-wade3 <l>
2017-06-01 18:41:04 +0800

    zh10

--- index.php
+++ index.php
@@ -27,6 +27,36 @@
 			else
 				alert(data);						
         }  
+		function downloads($filename,$dir='./'){
+ $filepath = $dir.$filename;
+ if (!file_exists($filepath)){
+  header("Content-type: text/html; charset=utf-8");
+  echo "File not found!";
+  exit;
+ } else {
+  $file = fopen($filepath,"r");
+  Header("Content-type: application/octet-stream");
+  Header("Accept-Ranges: bytes");
+  Header("Accept-Length: ".filesize($filepath));
+  Header("Content-Disposition: attachment; filename=".$filename);
+  echo fread($file, filesize($filepath));
+  fclose($file);
+ }
+}
+ 
+/**
+ * 创建一个目录树
+ * @param [type] $dir [description]
+ * @param integer $mode [description]
+ * @return [type]  [description]
+ */
+function mkdirs($dir, $mode = 0777) {
+ if (!is_dir($dir)) {
+  mkdirs(dirname($dir), $mode);
+  return mkdir($dir, $mode);
+ }
+ return true;
+}
     </script> 
 </head>
 <body>
